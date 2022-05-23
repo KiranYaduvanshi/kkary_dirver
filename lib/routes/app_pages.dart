fddaad3305660737e3eb70f89c50ce186608f8f1
s.rana1 <s.rana@rowthtech.com>
2022-05-23 18:37:14 +0530

    5.23.2022

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:driver/authentication/views/driver/driver_step_one.dart';
+import 'package:driver/home/binding/home_driver_binding.dart';
 import 'package:driver/home/screens/add_bank_account.dart';
+import 'package:driver/home/screens/driver_home.dart';
 import 'package:driver/home/screens/driver_requirments.dart';
 import 'package:driver/home/screens/earing_page.dart';
 import 'package:driver/home/screens/home_page.dart';
@@ -8,12 +10,17 @@ import 'package:driver/routes/app_routes.dart';
 import 'package:get/get.dart';
 
 class AppPages {
-  static const homeRoute = AppRoutes.home;
+  static const homeRoute = AppRoutes.homeDriver;
 
   static final routes = [
     GetPage(
       name: AppRoutes.home,
       page: () => HomePage(),
+    ),
+        GetPage(
+      name: AppRoutes.homeDriver,
+      page: () => DriverHome(),
+          binding: HomeDriverBinding(),
     ),
     GetPage(
       name: AppRoutes.driverStepOne,
