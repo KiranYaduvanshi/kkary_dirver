4762821e550516d6eb326d8002929bfbd7948874
kiranyaduvanshi <kirandeveloper786@gmail.com>
2022-05-24 12:12:43 +0530

    24 may-2022/12:12

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,4 +1,5 @@
 import 'package:driver/authentication/views/driver/driver_step_one.dart';
+import 'package:driver/authentication/views/driver/driver_step_two.dart';
 import 'package:driver/home/screens/add_bank_account.dart';
 import 'package:driver/home/screens/driver_requirments.dart';
 import 'package:driver/home/screens/earing_page.dart';
@@ -7,7 +8,7 @@ import 'package:driver/routes/app_routes.dart';
 import 'package:get/get.dart';
 
 class AppPages {
-  static const homeRoute = AppRoutes.home;
+  static const homeRoute = AppRoutes.driverStepOne;
 
   static final routes = [
     GetPage(
@@ -29,6 +30,10 @@ class AppPages {
     GetPage(
       name: AppRoutes.earing,
       page: () => EarningPage(),
+    ),
+    GetPage(
+      name: AppRoutes.driverStepTwo,
+      page: () => DriverStepTwo(),
     ),
   ];
 }
